073e1b42678e4bfd0dfe94ac51e718da5e6ff708
Thomas Chen <foxnewsnetwork@gmail.com>
2015-08-04 23:17:52 -0700

    added another account to the seed files

--- priv/repo/migrations/20150729230001_create_account.exs
+++ priv/repo/migrations/20150729230001_create_account.exs
@@ -17,27 +17,46 @@ defmodule Core.Repo.Migrations.CreateAccount do
       timestamps
     end
     create index(:accounts, [:user_id])
-    seed_account
+    seed_accounts
   end
 
   def down do
     drop table(:accounts)
   end
 
-  @seed_account %{
-    "user_id" => 1,
-    "company_name" => "Test Stage Co",
-    "access_key_id" => "AKIAINYEM24JX5TX33LA",
-    "secret_access_key" => "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",
-    "timezone" => "American/Los_Angeles",
-    "host" => "https://safe-forest-2497.herokuapp.com",
-    "namespace" => "apiv2",
-    "service_plan" => "test",
-    "uiux_host" => "https://simwms.github.io/uiux",
-    "config_host" => "https://simwms.github.io/config" }
-  defp seed_account do
+  @seed_accounts [
+    %{
+      "user_id" => 1,
+      "company_name" => "Test Stage Co",
+      "access_key_id" => "AKIAINYEM24JX5TX33LA",
+      "secret_access_key" => "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",
+      "timezone" => "American/Los_Angeles",
+      "host" => "https://safe-forest-2497.herokuapp.com",
+      "namespace" => "apiv2",
+      "service_plan" => "test",
+      "uiux_host" => "https://simwms.github.io/uiux",
+      "config_host" => "https://simwms.github.io/config" 
+    },
+    %{
+      "user_id" => 1,
+      "company_name" => "Test Local Co",
+      "access_key_id" => "AKIAINYEM24JX5TX33LA",
+      "secret_access_key" => "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",
+      "timezone" => "American/New_York",
+      "host" => "http://localhost:4000",
+      "namespace" => "apiv2",
+      "service_plan" => "test",
+      "uiux_host" => "https://simwms.github.io/uiux",
+      "config_host" => "https://simwms.github.io/config" 
+    }
+  ]
+  defp seed_accounts do
+    @seed_accounts
+    |> Enum.map(&seed_account/1)
+  end
+  defp seed_account(seed) do
     %Core.Account{}
-    |> Core.Account.changeset(@seed_account)
+    |> Core.Account.changeset(seed)
     |> Core.Repo.insert!
   end
 end
